e380fce3eb6a5839eace88ca82aa9a438e7836f4
MathiasMC <mathkot59@gmail.com>
2025-03-27 22:35:28 +0100

    1.21.5

--- src/main/java/dk/koefoedcloud/sleeptime/SleepTime.java
+++ src/main/java/dk/koefoedcloud/sleeptime/SleepTime.java
@@ -4,11 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 import org.lwjgl.glfw.GLFW;
 
@@ -16,12 +18,13 @@ public class SleepTime implements ClientModInitializer {
 
 	private KeyBinding toggleKey;
 	private boolean isOn = false;
+	private final Identifier LAYER = Identifier.of("sleeptime.client.layer");
 
 	@Override
 	public void onInitializeClient() {
 		MinecraftClient client = MinecraftClient.getInstance();
 		registerKey();
-		HudRenderCallback.EVENT.register(((drawContext, tickCounter) -> {
+		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.addLayer(IdentifiedLayer.of(LAYER,(drawContext, tickCounter) -> {
 			if (!isOn) {
 				return;
 			}
@@ -45,7 +48,7 @@ public class SleepTime implements ClientModInitializer {
 			long min = total / 60;
 			long sec = total % 60;
 			drawContext.drawText(client.textRenderer, min + ":" + sec, 1, 1, 0xFFFFFFFF, false);
-		}));
+		})));
 	}
 
 	private void registerClientTick() {
