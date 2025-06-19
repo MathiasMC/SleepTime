9108717cac00f16e810824f79ec62b9e3919bc97
MathiasMC <mathkot59@gmail.com>
2025-06-19 18:18:39 +0200

    1.21.6

--- src/main/java/dk/koefoedcloud/sleeptime/SleepTime.java
+++ src/main/java/dk/koefoedcloud/sleeptime/SleepTime.java
@@ -4,8 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
@@ -24,7 +23,7 @@ public class SleepTime implements ClientModInitializer {
 	public void onInitializeClient() {
 		MinecraftClient client = MinecraftClient.getInstance();
 		registerKey();
-		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.addLayer(IdentifiedLayer.of(LAYER,(drawContext, tickCounter) -> {
+		HudElementRegistry.addLast(LAYER, (drawContext, tickCounter) -> {
 			if (!isOn) {
 				return;
 			}
@@ -48,7 +47,7 @@ public class SleepTime implements ClientModInitializer {
 			long min = total / 60;
 			long sec = total % 60;
 			drawContext.drawText(client.textRenderer, min + ":" + sec, 1, 1, 0xFFFFFFFF, false);
-		})));
+		});
 	}
 
 	private void registerClientTick() {
